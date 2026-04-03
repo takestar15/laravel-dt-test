5cb05cb81327bacc895e44c8857896cd747d26bd
takestar15 <acmalit040195@gmail.com>
2026-04-03 04:23:19 +0000

    FIx lint, match all PHP versions

--- app/Http/Controllers/Api/PublicTranslationExportController.php
+++ app/Http/Controllers/Api/PublicTranslationExportController.php
@@ -6,8 +6,6 @@ use App\Actions\Translations\InvalidateTranslationCacheAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ExportTranslationsRequest;
 use App\Support\Translations\TranslationExport;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
 
